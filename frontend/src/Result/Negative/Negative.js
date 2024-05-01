3ae77bb5dcabd617ecdb5b2e8535d744e96976a8
huylamuit <20521389@gm.uit.edu.vn>
2024-05-01 13:14:33 +0700

    fix lib

--- frontend/src/Result/Negative/Negative.js
+++ frontend/src/Result/Negative/Negative.js
@@ -1,6 +1,15 @@
 import { Bar } from "react-chartjs-2";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { useEffect, useState } from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 ChartJS.register(
   CategoryScale,
   LinearScale,
